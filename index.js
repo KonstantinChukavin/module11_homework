46dbd705936832aae8269455b37cb59ee3c6b3be
KonstantinChukavin <KainkosYa@yandex.ru>
2022-10-22 17:54:27 +0300

    add task_1 home work 11

---
+++ index.js
@@ -0,0 +1,6 @@
+export function getPercents(percent, number) {
+    return number / 100 * percent;
+}
+
+// alert( getPercents(30, 200) );
+console.log(getPercents(1, 100));
---
+++ src/__tests__/task_1/index.tes.js
@@ -0,0 +1,18 @@
+
+import { getPercents } from "./index.js";
+
+describe("tests for getPercents function", () => {
+
+    it('corner-case', () =>
+        expect(getPercents(1, 100)).toBe(1));
+
+    it('percent is number', () =>
+        expect(getPercents(1, 100)).not.toBe(isNaN));
+
+    it('is range', () => {
+        expect(getPercents(1, 100)).toBeGreaterThan(0);
+        expect(getPercents(100, 100)).toBeLessThan(101);
+    });
+
+
+});
